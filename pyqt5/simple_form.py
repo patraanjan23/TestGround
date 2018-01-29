0516b504d0544b3d3866edce0c341d5e1e60c2a7
Dipanjan Patra <patraanjan23@gmail.com>
2018-01-29 14:27:43 +0530

    test#2
    made window frame-less and added window control buttons.
    
    made window corners round by overriding paintEvent.
    
    improved moving window algorithm by detecting if mouse
    is over the children.
    
    added stylesheet support.

--- pyqt5/simple_form.py
+++ pyqt5/simple_form.py
@@ -1,7 +1,6 @@
 import sys
 
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import QPoint
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 from form import Ui_Form
 
@@ -9,17 +8,55 @@ from form import Ui_Form
 class SimpleForm(QtWidgets.QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setupUi(self)
-        self.click_pt = QPoint()
-        self.drag_pt = QPoint()
+
+        self.click_pt = QtCore.QPoint()
+        self.drag_pt = QtCore.QPoint()
+
+        self.stylesheet = "stylesheet.css"
+        with open(self.stylesheet) as stylesheet:
+            self.setStyleSheet(stylesheet.read())
+
+        self.btnExit.clicked.connect(self.exit_app)
+        self.btnMax.clicked.connect(self.restore_app)
+        self.btnMin.clicked.connect(self.minimize_app)
+        self.btnSubmit.clicked.connect(self.print_text)
+
+    def print_text(self):
+        text = self.inputInfo.text()
+        print(text)
+
+    def minimize_app(self):
+        self.showMinimized()
+
+    def restore_app(self):
+        if self.isMaximized():
+            self.showNormal()
+        else:
+            self.showMaximized()
+
+    def exit_app(self):
+        self.close()
 
     def mousePressEvent(self, event):
-        self.click_pt = QPoint(event.x(), event.y())
+        self.click_pt = QtCore.QPoint(event.x(), event.y())
 
     def mouseMoveEvent(self, event):
-        self.drag_pt = QPoint(- self.click_pt.x() + self.x() + event.x(),
-                              - self.click_pt.y() + self.y() + event.y())
-        self.move(self.drag_pt)
+        children = self.findChildren((QtWidgets.QPushButton, QtWidgets.QLineEdit))
+        if True in map(lambda x: x.underMouse(), children):
+            pass
+        else:
+            self.drag_pt = QtCore.QPoint(- self.click_pt.x() + self.x() + event.x(),
+                                         - self.click_pt.y() + self.y() + event.y())
+            self.move(self.drag_pt)
+
+    def paintEvent(self, event):
+        path = QtGui.QPainterPath()
+        radius = 7
+        path.addRoundedRect(QtCore.QRectF(self.rect()), radius, radius)
+        mask = QtGui.QRegion(path.toFillPolygon().toPolygon())
+        self.setMask(mask)
 
 
 if __name__ == '__main__':
